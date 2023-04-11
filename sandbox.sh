6ec26b41e52bc97189bc0d3ad7c69f5647a03aee
Oleg Sinavski <sinavski@wayve.ai>
2023-04-11 14:45:22 +0100

    use current folder

--- sandbox.sh
+++ sandbox.sh
@@ -2,10 +2,10 @@
 set -e
 PROJECT_NAME=carla_ml
 
-echo "docker build -t $PROJECT_NAME ."
-
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ./docker_mlgl/stop_sandbox.sh $PROJECT_NAME
 # Build parent image
 ./docker_mlgl/build.sh mlgl_sandbox
-docker build -t $PROJECT_NAME .
-./docker_mlgl/start_sandbox.sh $PROJECT_NAME .
+docker build -t $PROJECT_NAME $SCRIPT_DIR
+./docker_mlgl/start_sandbox.sh $PROJECT_NAME $SCRIPT_DIR
